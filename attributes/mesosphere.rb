781709116ecb04684da8a07dfb6ca590c3974297
Steven Borrelli <steve@borrelli.org>
2013-10-08 15:44:22 -0500

    Add initial attributes for mesosphere packages

---
+++ attributes/mesosphere.rb
@@ -0,0 +1,7 @@
+default[:mesosphere][:version] =  "0.14.0" 
+default[:mesosphere]["0.14.0"]["ubuntu"]["12.04"][:url] =  "http://downloads.mesosphere.io/master/ubuntu/12.04/mesos_0.14.0_amd64.deb"
+default[:mesosphere]["0.14.0"]["ubuntu"]["12.10"][:url] =  "http://downloads.mesosphere.io/master/ubuntu/12.10/mesos_0.14.0_amd64.deb"
+default[:mesosphere]["0.14.0"]["centos"]["6.4"][:url] =  "http://downloads.mesosphere.io/master/centos/6.4/mesos_0.14.0_x86_64.rpm"
+default[:mesosphere]["0.14.0"]["redhat"]["6.4"][:url] =  "http://downloads.mesosphere.io/master/redhat/6.4/mesos_0.14.0_x86_64.rpm"
+default[:mesosphere]["0.14.0"]["debian"]["7.0"][:url] =  "http://downloads.mesosphere.io/master/debian/7.0/mesos_0.14.0_amd64.deb"
+default[:mesosphere]["0.14.0"]["osx"]["10.8"][:url] =  "http://downloads.mesosphere.io/master/osx/mesos-0.14.0.pkg"
