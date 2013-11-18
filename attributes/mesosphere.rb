e67c951c8b85b824f7d2781d310f69d9d3b5a066
Steven Borrelli <steve@borrelli.org>
2013-11-18 15:36:44 -0600

    Update mesos version to 0.14.2

--- attributes/mesosphere.rb
+++ attributes/mesosphere.rb
@@ -1,19 +1,19 @@
-default[:mesosphere][:version] =  "0.14.0-rc4" 
+default[:mesosphere][:version] =  "0.14.2" 
 
-default[:mesosphere]["0.14.0-rc4"]["ubuntu"]["12.04"][:url] =  "http://downloads.mesosphere.io/master/ubuntu/12.04/mesos_0.14.0-rc4_amd64.deb"
-default[:mesosphere]["0.14.0-rc4"]["ubuntu"]["12.04"][:checksum] = nil
+default[:mesosphere]["0.14.2"]["ubuntu"]["12.04"][:url] =  "http://downloads.mesosphere.io/master/ubuntu/12.04/mesos_0.14.2_amd64.deb"
+default[:mesosphere]["0.14.2"]["ubuntu"]["12.04"][:checksum] = nil
 
-default[:mesosphere]["0.14.0-rc4"]["ubuntu"]["12.10"][:url] =  "http://downloads.mesosphere.io/master/ubuntu/12.10/mesos_0.14.0-rc4_amd64.deb"
-default[:mesosphere]["0.14.0-rc4"]["ubuntu"]["12.10"][:checksum] = nil 
+default[:mesosphere]["0.14.2"]["ubuntu"]["12.10"][:url] =  "http://downloads.mesosphere.io/master/ubuntu/12.10/mesos_0.14.2_amd64.deb"
+default[:mesosphere]["0.14.2"]["ubuntu"]["12.10"][:checksum] = nil 
 
-default[:mesosphere]["0.14.0-rc4"]["centos"]["6.4"][:url] =  "http://downloads.mesosphere.io/master/centos/6.4/mesos_0.14.0-rc4_x86_64.rpm"
-default[:mesosphere]["0.14.0-rc4"]["centos"]["6.4"][:checksum] =  nil
+default[:mesosphere]["0.14.2"]["centos"]["6.4"][:url] =  "http://downloads.mesosphere.io/master/centos/6.4/mesos_0.14.2_x86_64.rpm"
+default[:mesosphere]["0.14.2"]["centos"]["6.4"][:checksum] =  nil
 
-default[:mesosphere]["0.14.0-rc4"]["redhat"]["6.4"][:url] =  "http://downloads.mesosphere.io/master/redhat/6.4/mesos_0.14.0-rc4_x86_64.rpm"
-default[:mesosphere]["0.14.0-rc4"]["redhat"]["6.4"][:checksum] =  nil
+default[:mesosphere]["0.14.2"]["redhat"]["6.4"][:url] =  "http://downloads.mesosphere.io/master/redhat/6.4/mesos_0.14.2_x86_64.rpm"
+default[:mesosphere]["0.14.2"]["redhat"]["6.4"][:checksum] =  nil
 
-default[:mesosphere]["0.14.0-rc4"]["debian"]["7.0"][:url] =  "http://downloads.mesosphere.io/master/debian/7.0/mesos_0.14.0-rc4_amd64.deb"
-default[:mesosphere]["0.14.0-rc4"]["debian"]["7.0"][:checksum] = nil
+default[:mesosphere]["0.14.2"]["debian"]["7.0"][:url] =  "http://downloads.mesosphere.io/master/debian/7.0/mesos_0.14.2_amd64.deb"
+default[:mesosphere]["0.14.2"]["debian"]["7.0"][:checksum] = nil
 
-default[:mesosphere]["0.14.0-rc4"]["osx"]["10.8"][:url] =  "http://downloads.mesosphere.io/master/osx/mesos-0.14.0-rc4.pkg"
-default[:mesosphere]["0.14.0-rc4"]["osx"]["10.8"][:checksum] = nil
+default[:mesosphere]["0.14.2"]["osx"]["10.8"][:url] =  "http://downloads.mesosphere.io/master/osx/mesos-0.14.2.pkg"
+default[:mesosphere]["0.14.2"]["osx"]["10.8"][:checksum] = nil
